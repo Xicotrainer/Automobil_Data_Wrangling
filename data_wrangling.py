3fce3d9df8850238af8ae38fd1f572edfcdea542
Xicotencatl Lopez <xicotrainer@gmail.com>
2021-04-24 21:59:52 -0500

    Dealing with a missing values. Steps: 5) Missing per columns and replace by 7) Mean or 8) Mode

--- data_wrangling.py
+++ data_wrangling.py
@@ -37,9 +37,33 @@ print(df.sample(8))
 df.replace("?", np.nan, inplace = True)
 print(df.sample(8))
 
-# Identify the proportion of missing values
+# Identify the number of missing values
 missing_data = df.isnull()
 print(missing_data.sample(8))
 for column in missing_data.columns.values.tolist():
 	print(column)
 	print(missing_data[column].value_counts(),"\n")
+
+# Replace by mean
+forChanging = ["normalized-losses", 
+				"bore",
+				"stroke",
+				"horsepower",
+				"peak-rpm"]
+
+for atribute in forChanging:
+	avg = df[atribute].astype("float").mean(axis = 0)
+	df[atribute].replace(np.nan, avg, inplace = True)
+	print("Average {}: {}".format(atribute, avg))
+
+# Replace by most common type
+mode = df["num-of-doors"].value_counts().idxmax()
+df["num-of-doors"].replace(np.nan, mode, inplace = True)
+
+# Drop every row without price and actualize the index
+df.dropna(subset = ["price"], axis = 0, inplace = True)
+df.reset_index(drop = True, inplace = True)
+
+# Now, the data has no missing values
+print(df.sample(8))
+print(df.dtypes)
