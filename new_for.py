c3c06c65c93cebf5f0138ed9b2d1f941fbc0abdc
Xicoténcatl Pérez López <Ecbalam_4to@hotmail.com>
2021-05-12 22:58:37 -0500

    For loop is really amazing especialy when it's used on array of tuples/functions, I love it

--- new_for.py
+++ new_for.py
@@ -1,10 +1,55 @@
-activos = [ 
-	("ES", 0.25, 50, "US"), 
-	("NQ", 0.25, 20, "US"),
-	("GC", 0.10, 100, "US"),
-	("DAX", 0.50, 50, "EUR")
-]
+import socket
 
-for activo, tick, costo_punto, moneda in activos:
-	print("El costo de cada punto em el {} es de {} {} y su minimo desplazamiento es de {} pts.".
-		format(activo, costo_punto, moneda, tick))
+
+def check_no_network():
+	"""Returns True if it fails to reslove broker's URL, False otherwise"""
+	try:
+		socket.gethostbyneme("https://www.binance.com")
+		return False
+	except:
+		return True
+
+
+def f_NQ():
+	print("This a NQ function")
+
+def f_ES():
+	print("Here is a ES momment")
+
+def f_CL():
+	print("Reciently the inventary has become simple new")
+
+def f_GC():
+	print("This asset is similar to the oil future")
+
+def f_DAX():
+	print("When I was a child, I realy loved it")
+
+
+# for by a lyst of funvtions
+
+def main():
+	activos = [ 
+		("ES", 0.25, 50, "US"), 
+		("NQ", 0.25, 20, "US"),
+		("CL", 0.10, 100, "US"),
+		("GC", 0.10, 100, "US"),
+		("DAX", 0.50, 50, "EUR")
+	]
+
+	for activo, tick, costo_punto, moneda in activos:
+		print("El costo de cada punto en el futuro del {} es de {} {} y su minimo desplazamiento es de {} pts.".
+			format(activo, costo_punto, moneda, tick))
+
+
+	f_list = [
+		(f_NQ, "Nasdaq"),
+		(f_ES, "S&P_500"),
+		(f_GC, "Gold")] 
+
+	for f_ , msg  in f_list:
+		
+		print("\n"+ msg + " Estamos mandado a llamar la función, esto es magico")
+		f_()
+
+main()
